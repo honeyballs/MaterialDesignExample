f952ad012bfb02c28200e5db2335a4f67e0b762d
Yannick Bals <yannick.bals@web.de>
2018-04-19 13:55:20 +0200

    Add Listeners

--- app/src/main/java/de/thm/materialdesignexample/MainActivity.java
+++ app/src/main/java/de/thm/materialdesignexample/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -32,7 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_layout);
 
         listView = findViewById(R.id.listView);
+        listView.setOnItemClickListener(new ListItemListener());
         button = findViewById(R.id.floatingButton);
+        button.setOnClickListener(new FABListener());
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -71,6 +75,22 @@ public class MainActivity extends AppCompatActivity {
         list = new ArrayList<>();
         for (int i = 0; i <= 100; i++) {
             list.add("Item no. " + i);
+        }
+    }
+
+    class FABListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Add Button clicked", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    class ListItemListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(MainActivity.this, list.get(position) + " clicked", Toast.LENGTH_SHORT).show();
         }
     }
 }

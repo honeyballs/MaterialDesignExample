9310c264f593bc137920e49d689d027c18113bcf
Yannick Bals <yannick.bals@web.de>
2018-04-19 13:15:24 +0200

    Set up colors, Themes and Layout

---
+++ app/src/main/java/de/thm/materialdesignexample/MainActivity.java
@@ -0,0 +1,19 @@
+package de.thm.materialdesignexample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yannick Bals on 19.04.2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    
+}
